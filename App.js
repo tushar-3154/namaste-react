e4744ab6e5ea8483d409e5fdbd166364b69f5e75
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-03-22 10:40:15 +0530

    component and jsx

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]), React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")])])
+// React.createElement => Object => HTMLElement(render)
 
-// const heanding = React.createElement("h1", { id: "heading" }, "Hello World From React");
+const heading = React.createElement("h1", { id: "heading" }, "Hello World 🌍");
 
-// console.log(heanding);
+// JSX - is not HTML in JS , HTML or XML like syntax
+// JSX (transpiled before it reaches the js) - parcel - Babel
+// const jsxheading = (<h1 className="heading">Namaste React using JSX </h1>)
+
+//React Functional Component
+
+// const HeadingComponent = () => {
+//     return <h1>Namaste React functional Component</h1>
+// }
+
+
+const ele = <span>React Element</span>
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">
+        {ele}
+        Namaste Title JSX
+    </h1>
+)
+
+
+// Component composition
+const HadingComponent2 = () => (
+    <div id="container">
+        {ele}
+        {Title()}
+        <Title />
+        <Title></Title>
+        <h1>Namste React functinal Component</h1>
+    </div>);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HadingComponent2 />);
