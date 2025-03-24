73cae6d5eed25b9044949c047baa9da6741f88ef
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-03-24 15:36:33 +0530

    create header and body in web site

--- App.js
+++ App.js
@@ -2,41 +2,483 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-// React.createElement => Object => HTMLElement(render)
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World 🌍");
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-// JSX - is not HTML in JS , HTML or XML like syntax
-// JSX (transpiled before it reaches the js) - parcel - Babel
-// const jsxheading = (<h1 className="heading">Namaste React using JSX </h1>)
-
-//React Functional Component
-
-// const HeadingComponent = () => {
-//     return <h1>Namaste React functional Component</h1>
+// const styleCard = {
+//     backgroundColor: "#f0f0f0"
 // }
 
+const resObj = [
+    {
+        "info": {
+            "id": "151649",
+            "name": "Hotel Sai Nath & Sai Restaurant",
+            "cloudinaryImageId": "vkhcohhmqfczycw9vsar",
+            "locality": "railway station",
+            "areaName": "Chhindwara Locality",
+            "costForTwo": "₹200 for two",
+            "cuisines": [
+                "North Indian",
+                "South Indian",
+                "Chinese",
+                "Beverages",
+                "Fast Food",
+                "Desserts"
+            ],
+            "avgRating": 4.3,
+            "veg": true,
+            "parentId": "101802",
+            "avgRatingString": "4.3",
+            "totalRatingsString": "976",
+            "sla": {
+                "deliveryTime": 45,
+                "lastMileTravel": 10.3,
+                "serviceability": "SERVICEABLE",
+                "slaString": "45-50 mins",
+                "lastMileTravelString": "10.3 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2025-03-24 22:30:00",
+                "opened": true
+            },
+            "badges": {
+                "imageBadges": [
+                    {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                    }
+                ]
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {},
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+                        "badgeObject": [
+                            {
+                                "attributes": {
+                                    "description": "pureveg",
+                                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                                }
+                            }
+                        ]
+                    },
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {},
+            "externalRatings": {
+                "aggregatedRating": {
+                    "rating": "--"
+                }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+            "context": "seo-data-9d655c96-8ad5-441f-a214-d6fa9e85a20a"
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/city/chhindwara/hotel-sai-nath-and-sai-restaurant-railway-station-chhindwara-locality-rest151649",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "151518",
+            "name": "Bakery World",
+            "cloudinaryImageId": "mt2aggiscfl3yviatwng",
+            "locality": "Parasia Road",
+            "areaName": "Parasia Road",
+            "costForTwo": "₹250 for two",
+            "cuisines": [
+                "Bakery",
+                "Ice Cream",
+                "Snacks",
+                "Beverages"
+            ],
+            "avgRating": 4.4,
+            "veg": true,
+            "parentId": "40363",
+            "avgRatingString": "4.4",
+            "totalRatingsString": "260",
+            "sla": {
+                "deliveryTime": 42,
+                "lastMileTravel": 11.7,
+                "serviceability": "SERVICEABLE",
+                "slaString": "40-45 mins",
+                "lastMileTravelString": "11.7 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2025-03-24 22:30:00",
+                "opened": true
+            },
+            "badges": {
+                "imageBadges": [
+                    {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                    }
+                ]
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {},
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+                        "badgeObject": [
+                            {
+                                "attributes": {
+                                    "description": "pureveg",
+                                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                                }
+                            }
+                        ]
+                    },
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {},
+            "externalRatings": {
+                "aggregatedRating": {
+                    "rating": "--"
+                }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+            "context": "seo-data-9d655c96-8ad5-441f-a214-d6fa9e85a20a"
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/city/chhindwara/bakery-world-parasia-road-rest151518",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "658210",
+            "name": "The Fusion Lounge",
+            "cloudinaryImageId": "fa4944f0cfdcbca2bec1f3ab8e3db3f7",
+            "locality": "Triloki nagar",
+            "areaName": "Railway Station",
+            "costForTwo": "₹300 for two",
+            "cuisines": [
+                "South Indian",
+                "Chinese",
+                "Beverages",
+                "Fast Food",
+                "Desserts"
+            ],
+            "avgRating": 4.2,
+            "parentId": "395453",
+            "avgRatingString": "4.2",
+            "totalRatingsString": "326",
+            "sla": {
+                "deliveryTime": 66,
+                "lastMileTravel": 10.2,
+                "serviceability": "SERVICEABLE",
+                "slaString": "65-70 mins",
+                "lastMileTravelString": "10.2 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2025-03-24 23:00:00",
+                "opened": true
+            },
+            "badges": {},
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {},
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "₹50 OFF",
+                "subHeader": "ABOVE ₹129",
+                "discountTag": "FLAT DEAL"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {},
+            "externalRatings": {
+                "aggregatedRating": {
+                    "rating": "--"
+                }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+            "context": "seo-data-9d655c96-8ad5-441f-a214-d6fa9e85a20a"
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/city/chhindwara/the-fusion-lounge-triloki-nagar-railway-station-rest658210",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "150591",
+            "name": "Satkar Restaurant",
+            "cloudinaryImageId": "rvxp5xbniat84r6efku2",
+            "locality": "Sinchai Colony",
+            "areaName": "Satkar Chowk",
+            "costForTwo": "₹250 for two",
+            "cuisines": [
+                "North Indian",
+                "South Indian",
+                "Indian",
+                "Salads",
+                "Desserts"
+            ],
+            "avgRating": 4.4,
+            "veg": true,
+            "parentId": "21553",
+            "avgRatingString": "4.4",
+            "totalRatingsString": "2.6K+",
+            "sla": {
+                "deliveryTime": 42,
+                "lastMileTravel": 11.1,
+                "serviceability": "SERVICEABLE",
+                "slaString": "40-45 mins",
+                "lastMileTravelString": "11.1 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2025-03-24 22:45:00",
+                "opened": true
+            },
+            "badges": {
+                "imageBadges": [
+                    {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                    }
+                ]
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {},
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+                        "badgeObject": [
+                            {
+                                "attributes": {
+                                    "description": "pureveg",
+                                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                                }
+                            }
+                        ]
+                    },
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {},
+            "externalRatings": {
+                "aggregatedRating": {
+                    "rating": "--"
+                }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+            "context": "seo-data-9d655c96-8ad5-441f-a214-d6fa9e85a20a"
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/city/chhindwara/satkar-restaurant-sinchai-colony-satkar-chowk-rest150591",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "151515",
+            "name": "Gupta Bhojnalay",
+            "cloudinaryImageId": "jo9pdipf4elcuch8g55q",
+            "locality": "fulwara chowk",
+            "areaName": "Chhindwara Locality",
+            "costForTwo": "₹200 for two",
+            "cuisines": [
+                "Fast Food",
+                "Indian",
+                "Beverages"
+            ],
+            "avgRating": 4,
+            "veg": true,
+            "parentId": "91635",
+            "avgRatingString": "4.0",
+            "totalRatingsString": "447",
+            "sla": {
+                "deliveryTime": 51,
+                "lastMileTravel": 10.8,
+                "serviceability": "SERVICEABLE",
+                "slaString": "50-55 mins",
+                "lastMileTravelString": "10.8 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2025-03-24 22:45:00",
+                "opened": true
+            },
+            "badges": {
+                "imageBadges": [
+                    {
+                        "imageId": "v1695133679/badges/Pure_Veg111.png",
+                        "description": "pureveg"
+                    }
+                ]
+            },
+            "isOpen": true,
+            "aggregatedDiscountInfoV2": {},
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {
+                        "badgeObject": [
+                            {
+                                "attributes": {
+                                    "description": "pureveg",
+                                    "imageId": "v1695133679/badges/Pure_Veg111.png"
+                                }
+                            }
+                        ]
+                    },
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {},
+            "externalRatings": {
+                "aggregatedRating": {
+                    "rating": "--"
+                }
+            },
+            "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+        },
+        "analytics": {
+            "context": "seo-data-9d655c96-8ad5-441f-a214-d6fa9e85a20a"
+        },
+        "cta": {
+            "link": "https://www.swiggy.com/city/chhindwara/gupta-bhojnalay-fulwara-chowk-chhindwara-locality-rest151515",
+            "type": "WEBLINK"
+        }
+    }
+]
 
-const ele = <span>React Element</span>
+const RestauratCard = (props) => {
 
-const Title = () => (
-    <h1 className="head" tabIndex="5">
-        {ele}
-        Namaste Title JSX
-    </h1>
-)
+    const { resData } = props;
 
+    const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } = resData.info;
 
-// Component composition
-const HadingComponent2 = () => (
-    <div id="container">
-        {ele}
-        {Title()}
-        <Title />
-        <Title></Title>
-        <h1>Namste React functinal Component</h1>
-    </div>);
+    const { deliveryTime } = resData.info.sla;
+
+    return (
+        <div className="res-card" style={{
+            backgroundColor: "#f0f0f0"
+        }}>
+            <img className="res-logo" alt="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}></img>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{deliveryTime} minutes</h4>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="Search">Search</div>
+            <div className="res-container">
+                {
+                    resObj.map((restaurant) => (
+                        <RestauratCard key={restaurant.info.id} resData={restaurant} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HadingComponent2 />);
+root.render(<AppLayout />);
